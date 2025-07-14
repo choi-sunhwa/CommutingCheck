fe37b94194bdfe0e9e8098f56ca7c69a9ace120f
choi sunhwa <sunhwa824@naver.com>
2025-07-14 14:40:03 +0900

    Initial commit

---
+++ CommutingCheck/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace CommutingCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Location = new System.Drawing.Point(0, 0);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(799, 454);
+            this.webView2.TabIndex = 0;
+            this.webView2.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+    }
+}
+
---
+++ CommutingCheck/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace CommutingCheck
+{
+    public partial class Form1 : Form
+    {
+        public static IConfiguration config;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            webView2.Source = new System.Uri($"{config["AccountInfo:url"]}", System.UriKind.Absolute);
+
+            WebLogin();
+        }
+
+        private async void WebLogin()
+        {
+            #region 혹시 몰라서 edge 닫음
+            await Task.Delay(1500);
+            Process[] mProcess = Process.GetProcessesByName("msedge");
+            if(mProcess.Length > 0) foreach (Process process in mProcess) process.Kill();
+            #endregion 혹시 몰라서 edge 닫음
+
+            #region 메인 로그인
+            await Task.Delay(1500);
+            webView2?.CoreWebView2.ExecuteScriptAsync($"document.getElementById('userId').value = '{config["AccountInfo:UserId"]}'");
+            webView2?.CoreWebView2.ExecuteScriptAsync($"document.getElementById('userPw').value = '{config["AccountInfo:Password"]}'");
+
+            await Task.Delay(1500);
+            await webView2.CoreWebView2.ExecuteScriptAsync("actionLogin()");
+            #endregion 메인 로그인
+
+            await Task.Delay(1500);
+            await webView2.CoreWebView2.ExecuteScriptAsync("secondCertLogin()");
+
+            await Task.Delay(5000);
+            DateTime now = DateTime.Now;
+            int dayOfWeek = Convert.ToInt32(now.DayOfWeek);
+            int hour = now.Hour;
+            if (dayOfWeek >= 1 && dayOfWeek <= 5) //월화수목금
+            {
+                if (hour == 9 || hour == 19 || hour == 23)
+                {
+                    bool _in = true;
+                    if (hour == 19 || hour == 23) _in = false;
+
+                    if (_in) webView2?.CoreWebView2.ExecuteScriptAsync("if(document.getElementById('portletTemplete_mybox_tab1').innerHTML.indexOf('없음') > -1) fnAttendCheck(1,0);");
+                    else
+                    {
+                        webView2?.CoreWebView2.ExecuteScriptAsync("document.getElementById('portletTemplete_mybox_tab1').style.display = 'none';");
+                        webView2?.CoreWebView2.ExecuteScriptAsync("document.getElementById('portletTemplete_mybox_tab2').style.display = 'block';");
+                        webView2?.CoreWebView2.ExecuteScriptAsync("if(document.getElementById('portletTemplete_mybox_tab2').innerHTML.indexOf('없음') > -1) fnAttendCheck(4,0);");
+                    }
+
+                    await Task.Delay(1500);
+                    webView2?.CoreWebView2.ExecuteScriptAsync("if(document.getElementById('btnConfirm')) document.getElementById('btnConfirm').click();");
+                }
+            }
+
+            await Task.Delay(1500);
+            mProcess = Process.GetProcessesByName("CommutingCheck");
+            if (mProcess.Length == 1)
+            {
+                foreach (Process process in mProcess) process.Kill();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
